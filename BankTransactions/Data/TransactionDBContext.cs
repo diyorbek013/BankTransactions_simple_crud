31af33ae11575df276bd619862502b0f0db0a71b
diyorbek00013@gmail.com <diyorbek00013@gmail.com>
2022-10-04 00:57:22 +0500

    Add project files.

---
+++ BankTransactions/Data/TransactionDBContext.cs
@@ -0,0 +1,14 @@
+﻿using BankTransactions.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankTransactions.Data
+{
+    public class TransactionDBContext : DbContext
+    {
+        public TransactionDBContext(DbContextOptions<TransactionDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
